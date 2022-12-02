5a4c33e02f21cdcdef76b7a58a8e2aefc50833df
yuli <yuliana@dormify.com>
2022-12-02 17:18:33 -0300

    profile

---
+++ Fundamentos de la web/JS/4 Hacer conexiones/script.js
@@ -0,0 +1,16 @@
+var request = 2;
+var connections = 418;
+
+function accept(index){
+    document.querySelector(`#request${index}`).remove();
+    request = request -1;
+    connections = connections+1;
+    document.querySelector("#request-count").innerText = request;
+    document.querySelector("#connections-count").innerText = connections;
+}
+
+function onremove(index){
+    document.querySelector(`#request${index}`).remove();
+    request = request -1;
+    document.querySelector("#request-count").innerText = request;
+}
